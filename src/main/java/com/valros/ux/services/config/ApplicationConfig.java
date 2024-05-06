16a59b7118d4faab7bcdf4208b64fbbd528d0205
César Valdiviezo <cvalros92@gmail.com>
2024-05-06 02:01:28 -0500

    add projects structure

---
+++ src/main/java/com/valros/ux/services/config/ApplicationConfig.java
@@ -0,0 +1,26 @@
+package com.valros.ux.services.config;
+
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * Main class for running Spring Boot framework.<br/>
+ * <b>Class</b>: EmployeeApplication<br/>
+ * <b>Copyright</b>: &copy; 2021 Valros Perú SAC<br/>
+ * <b>Company</b>: Valros Perú SAC<br/>
+ *
+ * @author ValRos Perú SAC <br/>
+ *
+ * <u>Developed by</u>: <br/>
+ * <ul>
+ * <li>C.A.V.R.</li>
+ * </ul>
+ * <u>Changes</u>:<br/>
+ * <ul>
+ * <li>April 15, 2021 Creaci&oacute;n de Clase.</li>
+ * </ul>
+ * @version 1.0
+ */
+
+@Configuration
+public class ApplicationConfig {
+}
---
+++ src/test/java/com/valros/ux/services/smartrash/controller/SmartrashControllerImplTests.java
@@ -0,0 +1,9 @@
+package com.valros.ux.services.smartrash.controller;
+
+import org.junit.runner.RunWith;
+import org.mockito.junit.MockitoJUnitRunner;
+
+@RunWith(MockitoJUnitRunner.class)
+public class SmartrashControllerImplTests {
+
+}
